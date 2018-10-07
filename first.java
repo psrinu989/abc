cef5ee914d0ae082249b6365fc49de019d19b6d5
srinivasulu <srini@gmail.com>
2018-10-07 16:15:51 +0530

    Second Commit

---
+++ first.java
@@ -0,0 +1,15 @@
+class first
+{
+	public static void main(String...args)
+	{
+		int a=10;	
+		if( a==10)
+		{
+			System.out.println("The Number is 10");
+		}
+		else
+		{
+			System.out.println("The Number is not 10");
+		}
+	}
+}
